ed680e0f7555e451e6f97a006a390e87626d7490
Marcus Vitek <marcus.vitek@phactum.at>
2024-12-30 19:14:45 +0100

    cleanup experiments
    from feature/federation-experiments merge

--- development/simulator/src/main/webapp-angular-native-module-federation-remote/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-
-import { routes } from './app.routes';
-import {provideRouter} from "@angular/router";
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)] // standalone routes (not used for remote access)
-}
--- development/simulator/src/main/webapp-angular-native/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {UserTaskDetailsComponent} from './user-tasks/user-task-detail/user-task-details.component';
-
-export const routes: Routes = [
-  {
-    path: 'tasks/:id',
-    component: UserTaskDetailsComponent
-  }
-];
